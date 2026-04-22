812d2474f699cf65a8bc97c6c4a4d08d7489c719
Oussama Mbarek <mbarekoussama3@gmail.com>
2026-04-22 23:48:54 +0100

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://qualiflow-backend-1.onrender.com',
   webPushPublicKey: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://qualiflow-backend-1.onrender.com',
   webPushPublicKey: ''
 };
